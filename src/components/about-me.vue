e974556fa43b424e9a4b202122514abb2e333015
Sebastian Selling <sebastian.selling@outlook.com>
2022-02-14 12:55:42 +0100

    about me and side projects sections updated

--- src/components/about-me.vue
+++ src/components/about-me.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<div class="large-container py-20 lg:py-40 md:py-30 mt-32">
+		<div class="py-20 mt-32 large-container lg:py-40 md:py-30">
 			<h2 class="text-4xl md:text-5xl lg:text-6xl">About me</h2>
 			<div class="grid gap-5 md:grid-cols-2 lg:grid-cols-2">
 				<div>
-					<p class="text-2xl font-light leading-relaxed max-w-lg">
+					<p class="max-w-lg text-2xl font-light leading-relaxed">
 						<br />
 						<span class="font-bold">Background</span>
 						<br />
@@ -14,37 +14,31 @@
 						path until much later in life and I'm very grateful to be where I am
 						- to make things for a living.
 					</p>
-					<p class="text-2xl font-light leading-relaxed max-w-xl mt-5">
+					<p class="max-w-xl mt-5 text-2xl font-light leading-relaxed">
 						<span class="font-bold">Career</span>
 						<br />
 						I've spent the last 10 years building products for the construction
 						industry and my main focus areas over the years have been in
 						business, product, branding and front-end development.
 					</p>
-					<p class="text-2xl font-light leading-relaxed max-w-xl mt-5">
-						<span class="font-bold">Going forward</span>
-						<br />
-						I'm currently a free agent and looking for a new home. I thrive in
-						the startup enviroment and love taking ideas from zero to one.
-						<br />
-            <div class="mt-5">
-              <a
-                href="https://calendly.com/sebastian-selling/30min"
-                class="rounded-full border-2 border-white hover:bg-gray-300 hover:text-black transition duration-300 ease-in-out text-lg py-2 px-4 font-medium"
-                target="_blank"
-                data-magnetic
-                data-cursor="-opaque"
-                >Let's have a chat!</a
-              >
-
-            </div>
-					</p>
+					<span class="max-w-xl mt-5 text-2xl font-light leading-relaxed">
+						<div class="mt-5">
+							<a
+								href="https://calendly.com/sebastian-selling/30min"
+								class="px-4 py-2 text-lg font-medium transition duration-300 ease-in-out border-2 border-white rounded-full hover:bg-gray-300 hover:text-black"
+								target="_blank"
+								data-magnetic
+								data-cursor="-opaque"
+								>Let's have a chat!</a
+							>
+						</div>
+					</span>
 				</div>
-				<div>
+				<div className="relative overflow-hidden">
 					<img
 						src="../assets/maincharacter.jpg"
 						alt=""
-						class="my-8 rounded-xl"
+						class="my-8 rounded-xl bg-gradient-to-t from-slate-900 to-transparent h-3/4"
 					/>
 				</div>
 			</div>
--- src/components/side-projects.vue
+++ src/components/side-projects.vue
@@ -6,28 +6,57 @@
 -->
 	<div class="large-container">
 		<div class="py-16 sm:py-24">
-			<h2 class="brief font-light lg:text-5xl md:text-3xl text-3xl">
-				Side projects
+			<h2 class="text-3xl font-light brief lg:text-5xl md:text-3xl">
+				Side projects / for practice
 			</h2>
-			<p class="text-lg py-5">Some work for fun or practice</p>
+			<p class="pt-5 text-2xl">Apps or products</p>
 
 			<div
-				class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+				class="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
 			>
 				<!-- post 0 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-72 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
+					>
+						<img
+							src="../assets/projects/hobby/seedvault.png"
+							alt="Netflix Clone"
+							class="object-cover object-center w-full rounded-2xl h-96"
+						/>
+					</div>
+					<div class="flex justify-between mt-4">
+						<div>
+							<h3 class="font-bold text-gray-100 font-2xl">
+								<a
+									href="https://tender-ritchie-77beda.netlify.app/"
+									target="_blank"
+								>
+									<span aria-hidden="true" class="absolute inset-3"
+										>Quick modern login page prototype</span
+									>
+								</a>
+							</h3>
+							<p class="mt-1 text-sm font-medium text-gray-300">
+								react + tailwind
+							</p>
+						</div>
+					</div>
+				</div>
+				<!-- post 0 -->
+				<div class="relative group">
+					<div
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<img
 							src="../assets/projects/hobby/globularexpirement.png"
 							alt="Netflix Clone"
-							class="w-full h-full object-center object-cover lg:w-full lg:h-full rounded-1xl"
+							class="object-cover object-center w-full rounded-2xl h-96"
 						/>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a
 									href="https://vigorous-lumiere-acf9a2.netlify.app/"
 									target="_blank"
@@ -37,43 +66,48 @@
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">
+							<p class="mt-1 text-sm font-medium text-gray-300">
 								ThreeJS landing page
 							</p>
 						</div>
 					</div>
 				</div>
 				<!-- post 1 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-72 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<img
 							src="../assets/projects/hobby/netflix_clone_cover.png"
 							alt="Netflix Clone"
-							class="w-full h-full object-center object-cover lg:w-full lg:h-full rounded-1xl"
+							class="object-cover object-center w-full rounded-2xl h-96"
 						/>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="https://netflix-clone-d8976.web.app/" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Netflix Clone</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">
+							<p class="mt-1 text-sm font-medium text-gray-300">
 								Can currently press a title and import trailers (albeit a bit
 								random)
 							</p>
 						</div>
 					</div>
 				</div>
+			</div>
+			<p class="pt-5 text-2xl">Creative coding</p>
+			<div
+				class="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+			>
 				<!-- post 2 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<video controls autoplay>
 							<source
@@ -83,23 +117,23 @@
 							Your browser does not support the video tag.
 						</video>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="#" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Creative coding - Network</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">Canvas.js</p>
+							<p class="mt-1 text-sm font-medium text-gray-300">Canvas.js</p>
 						</div>
 					</div>
 				</div>
 				<!-- post 3 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<video controls autoplay>
 							<source
@@ -109,46 +143,51 @@
 							Your browser does not support the video tag.
 						</video>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="#" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Creative coding - Grid of ghosts</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">Canvas.js</p>
+							<p class="mt-1 text-sm font-medium text-gray-300">Canvas.js</p>
 						</div>
 					</div>
 				</div>
 				<!-- post 4 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<video controls autoplay>
 							<source src="../assets/graphics/spirals.mp4" type="video/mp4" />
 							Your browser does not support the video tag.
 						</video>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="#" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Creative coding - Spirals</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">Canvas.js</p>
+							<p class="mt-1 text-sm font-medium text-gray-300">Canvas.js</p>
 						</div>
 					</div>
 				</div>
+			</div>
+			<p class="pt-5 text-2xl">Graphic / brand design</p>
+			<div
+				class="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+			>
 				<!-- post 5 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<img
 							src="../assets/graphics/sebastian_selling_logotype1080.png"
@@ -156,16 +195,16 @@
 							srcset=""
 						/>
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="#" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Personal logo iterations</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">
+							<p class="mt-1 text-sm font-medium text-gray-300">
 								Tried my best to stay very minimal with my brand and let my
 								content/work take center stage
 							</p>
@@ -173,22 +212,22 @@
 					</div>
 				</div>
 				<!-- post 5 -->
-				<div class="group relative">
+				<div class="relative group">
 					<div
-						class="w-full aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+						class="w-full overflow-hidden rounded-md aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:aspect-none"
 					>
 						<img src="../assets/graphics/gradiancelogo.png" alt="" srcset="" />
 					</div>
-					<div class="mt-4 flex justify-between">
+					<div class="flex justify-between mt-4">
 						<div>
-							<h3 class="text-gray-100 font-bold font-2xl">
+							<h3 class="font-bold text-gray-100 font-2xl">
 								<a href="#" target="_blank">
 									<span aria-hidden="true" class="absolute inset-3"
 										>Gradiance</span
 									>
 								</a>
 							</h3>
-							<p class="mt-1 text-sm text-gray-300 font-medium">
+							<p class="mt-1 text-sm font-medium text-gray-300">
 								Something that I worked on a friday evening that ended up as my
 								brand base and landing page header √
 							</p>
