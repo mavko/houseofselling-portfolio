af78c199befc07fd85074a2f43d76600f1c39fc3
Sebastian Selling <sebastian.selling@outlook.com>
2024-05-24 18:10:27 +0200

    nother one 5

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -5,38 +5,42 @@ import { useTheme } from 'next-themes'
 import createGlobe from 'cobe'
 
 const Globe = () => {
-  const canvasRef = useRef(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const phiRef = useRef(0)
   const { resolvedTheme } = useTheme()
 
   useEffect(() => {
     const darkMode = resolvedTheme === 'dark' ? 1 : 0
+    const canvas = canvasRef.current // Get the canvas element
 
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: 800 * 2,
-      height: 800 * 2,
-      phi: 0,
-      theta: 0,
-      dark: darkMode,
-      diffuse: 1.2,
-      mapSamples: 12000,
-      mapBrightness: 6,
-      baseColor: [0.7, 0.7, 0.7],
-      markerColor: [0.1, 0.1, 1],
-      glowColor: [0.5, 0.5, 0.5],
-      markers: [
-        { location: [37.7595, -122.4367], size: 0.03 },
-        { location: [59.334591, 18.06324], size: 0.1 },
-      ],
-      onRender: (state) => {
-        state.phi = phiRef.current
-        phiRef.current += 0.005
-      },
-    })
+    // Check if the canvas element is not null before calling createGlobe
+    if (canvas) {
+      const globe = createGlobe(canvas, {
+        devicePixelRatio: 2,
+        width: 800 * 2,
+        height: 800 * 2,
+        phi: 0,
+        theta: 0,
+        dark: darkMode,
+        diffuse: 1.2,
+        mapSamples: 12000,
+        mapBrightness: 6,
+        baseColor: [0.7, 0.7, 0.7],
+        markerColor: [0.1, 0.1, 1],
+        glowColor: [0.5, 0.5, 0.5],
+        markers: [
+          { location: [37.7595, -122.4367], size: 0.03 },
+          { location: [59.334591, 18.06324], size: 0.1 },
+        ],
+        onRender: (state) => {
+          state.phi = phiRef.current
+          phiRef.current += 0.005
+        },
+      })
 
-    return () => {
-      globe.destroy()
+      return () => {
+        globe.destroy()
+      }
     }
   }, [resolvedTheme])
 
