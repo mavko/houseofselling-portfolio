be25dfed7fdc62a91a558c0de7511a2ffaa3e098
Sebastian Selling <sebastian.selling@outlook.com>
2024-05-24 18:45:27 +0200

    nother 7

--- next.config.mjs
+++ next.config.mjs
@@ -14,5 +14,11 @@ const withMDX = nextMDX({
     rehypePlugins: [rehypePrism],
   },
 })
-
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 export default withMDX(nextConfig)
