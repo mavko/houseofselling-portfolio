6c626c82069daaf0465a148049fbe8e3edd4e971
Sebastian Selling <sebastian.selling@outlook.com>
2024-05-24 18:26:51 +0200

    added a typescript config file specifically for the typewriterjs animation

---
+++ typewriter-effect.d.ts
@@ -0,0 +1,18 @@
+declare module 'typewriter-effect/dist/core' {
+  import { TypewriterOptions } from 'typewriter-effect'
+
+  export default class Typewriter {
+    constructor(element: HTMLElement | null, options?: TypewriterOptions)
+    typeString(string: string): Typewriter
+    pauseFor(ms: number): Typewriter
+    start(): void
+    // Add any other methods you're using from the library
+  }
+
+  export interface TypewriterOptions {
+    // Add the options interface properties here
+    loop?: boolean
+    delay?: number
+    // ...
+  }
+}
