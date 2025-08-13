99defd5bc75083ed3986d60a40e920d6e2ba782c
Sebastian Selling <sebastian.selling@outlook.com>
2025-08-13 15:08:02 +0200

    Update Home component layout and integrate Shadows for enhanced visual design
    - Replaced the previous layout with a centered flexbox design for improved aesthetics.
    - Integrated the Shadows component to add depth and animation effects to the Home page.
    - Removed outdated links and text to streamline the content presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,18 @@ import React from 'react'
 import ArtifactsContent from './artifacts/ArifactsContent'
 import DialogComponent from '@/components/Dialog'
 import Link from 'next/link'
+import { Shadows } from '@/components/Shadows'
 
 export default function Home() {
   return (
     <>
-      <div className="space-y-6 py-16 lg:py-24">
-        <div className="flex items-center gap-6">
-          <Link
-            className="font-mono text-xs font-semibold tracking-tight text-white uppercase underline underline-offset-4"
-            href="/artifacts/archive/makings-of-ispect"
-          >
-            Makings of iSpect ↗
-          </Link>
-          <Link
-            className="font-mono text-xs font-semibold tracking-tight text-white uppercase underline underline-offset-4"
-            href="/artifacts/archive/makings-of-icontrol"
-          >
-            Makings of iControl ↗
-          </Link>
-        </div>
-        <h1 className="text-5xl/12 font-semibold tracking-tight text-balance text-white sm:text-5xl/14">
-          Designer and builder from Sweden with a passion spanning a decade+ of
-          taking ideas from zero to one.
-        </h1>
-        <div className="flex items-center gap-4">
-          <DialogComponent />
-        </div>
+      <div className="flex h-screen w-full items-center justify-center border border-white/10">
+        <Shadows
+          color="rgba(128, 128, 128, 1)"
+          animation={{ scale: 100, speed: 90 }}
+          noise={{ opacity: 1, scale: 1.2 }}
+          sizing="fill"
+        />
       </div>
 
       <ArtifactsContent />
